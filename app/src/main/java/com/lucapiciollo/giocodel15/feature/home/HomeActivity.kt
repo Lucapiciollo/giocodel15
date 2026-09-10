88c2bfc93c367ceec6f31c2141b5215aa238fcdc
Lucapiciollo <Lucapiciollo@gmail.com>
2026-09-10 19:34:59 +0200

    feat(home): wire solo game and settings actions

--- app/src/main/java/com/lucapiciollo/giocodel15/feature/home/HomeActivity.kt
+++ app/src/main/java/com/lucapiciollo/giocodel15/feature/home/HomeActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.lucapiciollo.giocodel15.databinding.ActivityHomeBinding
 import com.lucapiciollo.giocodel15.feature.create.CreateTableActivity
+import com.lucapiciollo.giocodel15.feature.game.GameActivity
 import com.lucapiciollo.giocodel15.feature.nearby.NearbyTablesActivity
+import com.lucapiciollo.giocodel15.feature.settings.SettingsActivity
 
 class HomeActivity : AppCompatActivity() {
 
@@ -19,9 +21,14 @@ class HomeActivity : AppCompatActivity() {
         binding.createTableButton.setOnClickListener {
             startActivity(Intent(this, CreateTableActivity::class.java))
         }
-
         binding.joinTableButton.setOnClickListener {
             startActivity(Intent(this, NearbyTablesActivity::class.java))
+        }
+        binding.soloGameButton.setOnClickListener {
+            startActivity(Intent(this, GameActivity::class.java))
+        }
+        binding.settingsButton.setOnClickListener {
+            startActivity(Intent(this, SettingsActivity::class.java))
         }
     }
 }
