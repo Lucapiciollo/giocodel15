c91c0b886b5ccf10ca0d2f44af4d535202658df6
Lucapiciollo <Lucapiciollo@gmail.com>
2026-09-10 14:14:06 +0200

    refactor: align game config with table settings

--- app/src/main/java/com/lucapiciollo/giocodel15/multiplayer/model/GameConfig.kt
+++ app/src/main/java/com/lucapiciollo/giocodel15/multiplayer/model/GameConfig.kt
@@ -3,6 +3,7 @@ package com.lucapiciollo.giocodel15.multiplayer.model
 data class GameConfig(
     val gridSize: Int,
     val seed: Long,
-    val maxPlayers: Int = 8,
-    val targetWins: Int = 1
+    val tableMode: TableMode = TableMode.TABLE,
+    val maxPlayers: Int = 4,
+    val targetWins: Int = 3
 )
