d2a9ea767700a7297f441c5332ec8bf0674671a1
Lucapiciollo <Lucapiciollo@gmail.com>
2026-09-12 15:22:52 +0200

    fix: align desugaring version with Nearby requirements

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
 }
 
 dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.3")
 
     implementation("androidx.core:core-ktx:1.18.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
