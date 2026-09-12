9fd1381bb12648553efeb100ca48fce98bdc4207
Luca Piciollo <lucapiciollo@gmail.com>
2026-09-12 17:38:40 +0200

    chore: bump version to 4 (0.4.0)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.lucapiciollo.giocodel15"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.4.0"
     }
 
     buildFeatures {
